913af639cef7f7ced3fc5953ad03974ac8b54a26
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-03-30 17:48:36 +0200

    Added Starship Controller

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipController extends AbstractController
+{
+    #[NoReturn] #[Route('/starships/{id<\d+>}')]
+    public function show(int $id, StarshipRepository $repository): Response
+    {
+        $ship = $repository->find($id);
+
+        if (!$ship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
+    }
+}
