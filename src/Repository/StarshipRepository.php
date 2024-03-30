29d11db58f5b8481da6f15baba9789ebbdd6c682
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-03-30 15:20:01 +0200

    Added ShipmentRepository

---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+
+class StarshipRepository
+{
+    public function findAll(): array
+    {
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q',
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+        ];
+    }
+}
