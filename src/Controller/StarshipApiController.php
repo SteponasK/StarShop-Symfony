6d2ab852c202e6cbe86d9bfac11a52e979a36fbc
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-03-30 15:43:42 +0200

    Added /api/starships/{id} route. <\d+> means that any digit

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
 use App\Repository\StarshipRepository;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,5 +15,11 @@ class StarshipApiController extends AbstractController
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/api/starships/{id<\d+>}')]
+    public function get(int $id): Response
+    {
+        dd($id);
     }
 }
