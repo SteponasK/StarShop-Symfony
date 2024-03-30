0a8607d3655fb423b0b7227a20a9de2089482a8c
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-03-30 17:56:49 +0200

    Removes noreturn attribute from route

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
-use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController
 {
-    #[NoReturn] #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
+    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
     public function show(int $id, StarshipRepository $repository): Response
     {
         $ship = $repository->find($id);
