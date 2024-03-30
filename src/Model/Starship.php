0f9fc109921ba401c991c7d6a7ff4d5ea1e467e4
SteponasK <132608893+SteponasK@users.noreply.github.com>
2024-03-30 19:34:26 +0200

    Added getStatusImageFilename method to load images in .twig easier

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -38,8 +38,17 @@ class Starship
         return $this->status;
     }
 
-    public function  getStatusString(): string
+    public function getStatusString(): string
     {
         return $this->status->value;
+    }
+
+    public function getStatusImageFilename(): string
+    {
+        return match ($this->status) {
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+        };
     }
 }
